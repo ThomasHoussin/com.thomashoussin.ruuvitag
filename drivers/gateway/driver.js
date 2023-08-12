a4d3bdb6ceb465558bf1d01d503a8d13fe2e4ddd
Thomas Houssin <thomas.houssin@gmail.com>
2023-08-13 01:40:26 +0200

    First version with Ruuvi Gateway Support (WIP)
     Ruuvi Gateway Support (WIP)
    Fixed trigger when entering / leaving range

--- drivers/gateway/driver.js
+++ drivers/gateway/driver.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fn = require('../../lib/function.js');
+const fetch = require('node-fetch');
 const { Driver } = require('homey');
 
 class MyDriver extends Driver {
@@ -8,29 +10,97 @@ class MyDriver extends Driver {
    * onInit is called when the driver is initialized.
    */
   async onInit() {
-    this.log('MyDriver has been initialized');
-  }
+      this.log('MyDriver has been initialized');
 
-  /**
-   * onPairListDevices is called when a user is adding a device
-   * and the 'list_devices' view is called.
-   * This should return an array with the data of devices that are available for pairing.
-   */
-  async onPairListDevices() {
-    return [
-      // Example device data, note that `store` is optional
-      // {
-      //   name: 'My Device',
-      //   data: {
-      //     id: 'my-device',
-      //   },
-      //   store: {
-      //     address: '127.0.0.1',
-      //   },
-      // },
-    ];
-  }
+      // Registering trigger
+      this.RuuviTagEnteredRange = this.homey.flow.getDeviceTriggerCard('ruuvitag_entered_range');
+      this.RuuviTagExitedRange = this.homey.flow.getDeviceTriggerCard('ruuvitag_exited_range');
 
+    }
+
+    onPair(session) {
+        var devices;
+        const discoveryStrategy = this.getDiscoveryStrategy();
+        const discoveryResults = discoveryStrategy.getDiscoveryResults();
+
+        //TODO : support more than  1 gateway
+        const discoveryResult = Object.values(discoveryResults)[0];
+
+        session.setHandler('validate', async function (data) {
+            console.log("Validate connection bearer token");
+            let token = data.token;
+
+            const validationUrl = `http://${discoveryResult.host}.local/history`;
+            const requestHeaders = new fetch.Headers({
+                "Authorization": `Bearer ${token}`
+            });
+
+            console.log(validationUrl);
+
+            return fetch(validationUrl, {
+                headers: requestHeaders
+            })
+                .then(checkResponseStatus)
+                .then(result => result.json())
+                .then(json => Object.keys(json.data.tags).map(id => {
+                    let buffer = Buffer.from(json.data.tags[id].data.substring(10),'hex') ;
+                    let new_device = {
+                        name: id,
+                        data: {
+                            id: id,
+                            hostname: discoveryResult.host ,
+                            dataformat: fn.readFormat(buffer) 
+                        },
+                        capabilities: [
+                            'measure_battery',
+                            'measure_temperature',
+                            'measure_rssi',
+                            'acceleration',
+                            'onoff'
+                        ],
+                        settings: {
+                            // Store bearer token
+                            token,
+                        },
+                    };
+
+                    // do not add capabilities not supported for Ruuvitag pro
+                    if (new_device.data.dataformat != 5 || fn.readPressure(new_device.data.dataformat, buffer) != 65535) new_device.capabilities.push('measure_pressure');
+                    if (new_device.data.dataformat != 5 || fn.readHumidity(new_device.data.dataformat, buffer) != 65535) new_device.capabilities.push('measure_humidity');
+
+                    if (new_device.data.dataformat == 5) {
+                        new_device.capabilities.push('alarm_motion');
+                        new_device.capabilities.push('alarm_battery');
+                        new_device.capabilities.push('button.resetbattery');
+                    }
+                    return new_device;
+                }))
+                .then(list => {
+                        devices = list;
+                        return 'ok';
+                })
+                .catch(error => {
+                        console.log(error);
+                        return error;
+                });
+        });
+
+        session.setHandler('list_devices', async function (data) {
+            console.log('List devices started...');
+
+            return devices;
+        });
+    }
 }
 
 module.exports = MyDriver;
+
+function checkResponseStatus(res) {
+    if (res.ok) {
+        return res
+    } else {
+        console.log(`Wrong response status : ${res.status} (${res.statusText})`);
+        throw new Error(`Wrong response status : ${res.status} (${res.statusText})`);
+    }
+}
+
